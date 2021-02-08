466d8dfbf85b0bdfc674160e075676f888663428
okeycsy <43840627+okeycsy@users.noreply.github.com>
2021-02-08 13:35:57 +0900

    0208

---
+++ EVproject/EVapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    template = loader.get_template('EVapp/index.html')
+    return HttpResponse(template.render(None, request))
+def car(request):
+    template = loader.get_template('EVapp/car.html')
+    return HttpResponse(template.render(None, request))
